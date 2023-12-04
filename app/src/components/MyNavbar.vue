f0954d9ddb49b76d777b518669b3b2c01441f97e
Juhana Viitamo <juhana.viitamo@efecte.com>
2023-12-04 20:48:43 +0200

    Update current link with bold

--- app/src/components/MyNavbar.vue
+++ app/src/components/MyNavbar.vue
@@ -15,10 +15,10 @@
       
       <div class="navbar-menu">
         <div class="navbar-desktop">
-          <router-link to="/" class="navbar-item">{{ $t('front_page') }}</router-link>
-          <router-link to="/events" class="navbar-item">{{ $t('events') }}</router-link>
-          <router-link class="navbar-item" to="/guild">{{ $t('for_guild') }}</router-link>
-          <router-link class="navbar-item" to="/contact">{{ $t('contact_information') }}</router-link>
+          <router-link to="/" class="navbar-item" :style="[ currentRoute('/') ? 'font-weight: bold' : '']">{{ $t('front_page') }}</router-link>
+          <router-link to="/events" class="navbar-item" :style="[ currentRoute('/events') ? 'font-weight: bold' : '']">{{ $t('events') }}</router-link>
+          <router-link class="navbar-item" to="/guild" :style="[ currentRoute('/guild') ? 'font-weight: bold' : '']">{{ $t('for_guild') }}</router-link>
+          <router-link class="navbar-item" to="/contact" :style="[ currentRoute('/contact') ? 'font-weight: bold' : '']">{{ $t('contact_information') }}</router-link>
         </div>
 
         <!-- Mobile menu button -->
@@ -30,10 +30,10 @@
 
         <!-- Mobile menu content -->
         <div class="navbar-mobile" v-if="isMobileMenuOpen">
-          <router-link @click="toggleMobileMenu" to="/" class="navbar-item">{{ $t('front_page') }}</router-link>
-          <router-link @click="toggleMobileMenu" to="/events" class="navbar-item">{{ $t('events') }}</router-link>
-          <router-link @click="toggleMobileMenu" class="navbar-item" to="/guild">{{ $t('for_guild') }}</router-link>
-          <router-link @click="toggleMobileMenu" class="navbar-item" to="/contact">{{ $t('contact_information') }}</router-link>
+          <router-link @click="toggleMobileMenu" to="/" class="navbar-item" :style="[ currentRoute('/') ? 'font-weight: bold' : '']">{{ $t('front_page') }}</router-link>
+          <router-link @click="toggleMobileMenu" to="/events" class="navbar-item" :style="[ currentRoute('/events') ? 'font-weight: bold' : '']">{{ $t('events') }}</router-link>
+          <router-link @click="toggleMobileMenu" class="navbar-item" to="/guild" :style="[ currentRoute('/guild') ? 'font-weight: bold' : '']">{{ $t('for_guild') }}</router-link>
+          <router-link @click="toggleMobileMenu" class="navbar-item" to="/contact" :style="[ currentRoute('/contact') ? 'font-weight: bold' : '']">{{ $t('contact_information') }}</router-link>
         </div>
       </div>
     </div>
@@ -54,6 +54,9 @@ export default {
     },
     switchLanguage(lang) {
       this.$i18n.locale = lang;
+    },
+    currentRoute(route) {
+      return this.$router.currentRoute.value.fullPath === route
     }
   },
 };
