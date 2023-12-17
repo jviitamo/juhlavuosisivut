bab30dea53b1dabf6d3e6697bd112755f6f09558
Juhana Viitamo <juhana.viitamo@efecte.com>
2023-12-17 12:32:45 +0200

    for guild page

--- app/src/components/ForGuild/ForGuild.vue
+++ app/src/components/ForGuild/ForGuild.vue
@@ -1,20 +1,17 @@
 <!-- MainBody.vue -->
 
 <template>
-    <div class="main-body-container">
-      <h1 class="main-heading">{{ $t('nav_bar[0].for_guild_header') }}</h1>
-      <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-        Where does it come from?
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-        
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-        
-        Where can I get some?
-        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+  <div class="main-body-container-for-guild">
+    <div class="for-guild-header">
+        <h1>{{ $t('main_page[0].main_page_6') }}</h1>
+        <div class="header-container">
+          <img src="@/assets/pylvas.svg" alt="Logo" />
+          <p> {{ $t('for_guild[0].text_0') }} <router-link to="/contact" class="link-bold-prevent-default">{{ $t('for_guild[0].text_1') }}</router-link> {{ $t('for_guild[0].text_2') }} </p>
+          <img src="@/assets/pylvas.svg" alt="Logo" />
+        </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
   
@@ -24,3 +21,70 @@
     }
   };
   </script>
+
+<style scoped>
+/* Add your custom styles for MainBody here */
+
+.main-body-container-for-guild {
+  width: 80%;
+  margin: 100px auto;
+  padding: 50px 0;
+}
+
+.for-guild-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  font-family: 'Magra';
+}
+
+
+.for-guild-header > h1 {
+  font-size: 4.5rem;
+  font-weight: bold;
+  max-width: 90%;
+  margin: 50px auto;
+}
+
+.header-container > img {
+  height: 500px;
+}
+
+.header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 90%;
+  margin: auto;
+}
+.header-container > p {
+  font-size: 2rem;
+  width: 70%;
+}
+
+@media screen and (max-width: 800px) {
+
+  .main-body-container-for-guild {
+    width: 100%;
+    margin: 40px auto;
+  }
+
+  .header-container {
+    width: 100%;
+  }
+
+  .header-container > img {
+    height: 300px;
+  }
+  
+  .for-guild-header > h1 {
+    font-size: 2rem;
+  }
+  
+  .header-container > p {
+    font-size: 1rem;
+  }
+}
+
+</style>
