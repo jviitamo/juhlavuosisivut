b28f86194b6a890b2b986dbb5f640d92f5410048
jviitamo <jviitamo@gmail.com>
2024-01-03 10:08:01 +0000

    New contact photos

--- app/src/components/ContactInformation/ContactInformation.vue
+++ app/src/components/ContactInformation/ContactInformation.vue
@@ -4,15 +4,15 @@
     <div class="main-body-container">
       <h1 class="main-heading-center">{{ $t('nav_bar[0].contact_information_header') }}</h1>
         <div class="box-container">
-          <BoxComponent name="Julia Sippala" email="julia.sippala@aalto.fi" telegram="@soppala" :title="$t('contact[0].responsible')" />
-          <BoxComponent name="Antti Kari-Koskinen" email="antti.kari-koskinen@aalto.fi" photoUrl="antti.jpeg" telegram="@teekkarikoskinen" :title="$t('contact[0].companies')" />
-          <BoxComponent name="Aarne Talvela" email="aarne.talvela@aalto.fi" telegram="@aarnetalvela" :title="$t('contact[0].events')" />
-          <BoxComponent name="Nea Nieminen" email="nea.nieminen@aalto.fi" telegram="@poromuija" :title="$t('contact[0].events')" />
-          <BoxComponent name="Elina Ståhlberg" email="elina.stahlberg@aalto.fi" photoUrl="ellun kuva.jpg" telegram="@stahlbergg" :title="$t('contact[0].graphics')" />
+          <BoxComponent name="Julia Sippala" email="julia.sippala@aalto.fi" photoUrl="julia.jpg" telegram="@soppala" :title="$t('contact[0].responsible')" />
+          <BoxComponent name="Antti Kari-Koskinen" email="antti.kari-koskinen@aalto.fi" photoUrl="antti.jpg" telegram="@teekkarikoskinen" :title="$t('contact[0].companies')" />
+          <BoxComponent name="Aarne Talvela" email="aarne.talvela@aalto.fi" photoUrl="aarne.jpg" telegram="@aarnetalvela" :title="$t('contact[0].events')" />
+          <BoxComponent name="Nea Nieminen" email="nea.nieminen@aalto.fi" photoUrl="nea.jpg" telegram="@poromuija" :title="$t('contact[0].events')" />
+          <BoxComponent name="Elina Ståhlberg" email="elina.stahlberg@aalto.fi" photoUrl="ellu.jpg" telegram="@stahlbergg" :title="$t('contact[0].graphics')" />
           <BoxComponent name="Iida Lehikoinen" email="iida.lehikoinen@aalto.fi" photoUrl="iida.png" telegram="@iidalehikoinen" :title="$t('contact[0].communication')" />
-          <BoxComponent name="Inkeri Nora" email="inkeri.nora@aalto.fi" telegram="@inkuri" :title="$t('contact[0].seminar')" />
-          <BoxComponent name="Niklas Koskela" email="niklas.koskela@aalto.fi" telegram="@Nicodeemus" :title="$t('contact[0].seminar')" />
-          <BoxComponent name="Juhana Viitamo" email="juhana.viitamo@aalto.fi" telegram="@juhanaviitamo" :title="$t('contact[0].web')" />
+          <BoxComponent name="Inkeri Nora" email="inkeri.nora@aalto.fi" photoUrl="inkeri.jpg" telegram="@inkuri" :title="$t('contact[0].seminar')" />
+          <BoxComponent name="Niklas Koskela" email="niklas.koskela@aalto.fi" photoUrl="niklas.jpg" telegram="@Nicodeemus" :title="$t('contact[0].seminar')" />
+          <BoxComponent name="Juhana Viitamo" email="juhana.viitamo@aalto.fi" photoUrl="juhana.jpg" telegram="@juhanaviitamo" :title="$t('contact[0].web')" />
         </div>
     </div>
   </template>
