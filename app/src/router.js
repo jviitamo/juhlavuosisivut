06296f93676db473d37507c04151c771e3c3d792
jviitamo <jviitamo@gmail.com>
2024-01-03 05:53:28 +0000

    Start scrolling from top after link

--- app/src/router.js
+++ app/src/router.js
@@ -55,6 +55,10 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    // always scroll to top
+    return { top: 0 }
+  },
 });
 
 export default router;
