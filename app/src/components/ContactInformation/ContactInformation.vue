0df8c3d434c66928d95d6fb4a1a5957d9f955628
jviitamo <jviitamo@gmail.com>
2024-01-12 07:39:45 +0000

    Update texts

--- app/src/components/ContactInformation/ContactInformation.vue
+++ app/src/components/ContactInformation/ContactInformation.vue
@@ -11,7 +11,7 @@
           <BoxComponent name="Elina Ståhlberg" email="elina.stahlberg@aalto.fi" photoUrl="ellu.jpg" telegram="@stahlbergg" :title="$t('contact[0].graphics')" />
           <BoxComponent name="Iida Lehikoinen" email="iida.lehikoinen@aalto.fi" photoUrl="iida.png" telegram="@iidalehikoinen" :title="$t('contact[0].communication')" />
           <BoxComponent name="Inkeri Nora" email="inkeri.nora@aalto.fi" photoUrl="inkeri.jpg" telegram="@inkuri" :title="$t('contact[0].seminar')" />
-          <BoxComponent name="Niklas Koskela" email="niklas.koskela@aalto.fi" photoUrl="niklas.jpg" telegram="@Nicodeemus" :title="$t('contact[0].seminar')" />
+          <BoxComponent name="Niklas Koskela" email="niklas.koskela@aalto.fi" photoUrl="niklas.jpg" telegram="@Viestintapaallikko" :title="$t('contact[0].seminar')" />
           <BoxComponent name="Juhana Viitamo" email="juhana.viitamo@aalto.fi" photoUrl="juhana.jpg" telegram="@juhanaviitamo" :title="$t('contact[0].web')" musicSrc="sound.mp3" />
         </div>
     </div>
